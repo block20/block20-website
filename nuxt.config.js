c7f9e08faad4a5de1e5c13ace08787635f7c2a52
Kevin Xu <kevin@imkevinxu.com>
2018-02-28 03:14:37 -0800

    Update root-level files

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,53 +1,65 @@
-const path = require('path')
-
-
 module.exports = {
-  /*
-  ** Headers of the page
-  */
+
+  // Header metadata
+
   head: {
-    title: 'BlockTwenty - Building and Funding the Protocols of Tomorrow',
+    title: 'Block 20 Group – Building and Funding the Protocols of Tomorrow',
+
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'keywords', content: 'blockchain, cryptocurrency, investments, ICO, token, smart contracts'},
-      { hid: 'description', name: 'description', content: 'We help build and fund the protocols of tomorrow' },
-      { hid: 'og:image', property: 'og:image', content: 'https://stark-beach-40759.herokuapp.com/blocktwenty_fb_og_image.png' }
+      { name: 'description', hid: 'description', content: 'We help build and fund the protocols of tomorrow' },
+
+      // Open graph
+      { name: 'og:title', content: 'Block 20 Group' },
+      { name: 'og:description', content: 'We help build and fund the protocols of tomorrow' },
+      { name: 'og:type', content: 'website' },
+      { name: 'og:url', content: 'https://blocktwenty.com/' },
+      { name: 'og:image', content: 'https://blocktwenty.com/og-image.png' },
+
+      // Twitter card
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:site', content: '@Block20Group' },
+      { name: 'twitter:title', content: 'Block 20 Group' },
+      { name: 'twitter:description', content: 'We help build and fund the protocols of tomorrow' },
+      { name: 'twitter:image', content: 'https://blocktwenty.com/og-image.png' },
     ],
+
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  /*
-  ** Customize the progress bar color
-  */
- loading: { color: '#FFFFFF' },
- /*
- ** Build configuration
- */
- build: {
-   /*
-   ** Run ESLint on save
-   */
-   extend (config, { isDev, isClient }) {
-     if (isDev && isClient) {
-       config.module.rules.push({
-         enforce: 'pre',
-         test: /\.(js|vue)$/,
-         loader: 'eslint-loader',
-         exclude: /(node_modules)/
-       })
-     }
-   }
- },
- modules: [
-   ['nuxt-sass-resources-loader', path.resolve(__dirname, './assets/_variables.scss')],
-   '@nuxtjs/font-awesome',
-   '@nuxtjs/optimize'
- ],
- plugins: [
-   { src: '~plugins/typeform.js', ssr: false },
-   { src: '~plugins/webFontLoader.js', ssr: false },
-   '~/plugins/vue-scrollto.js'
- ]
+
+  // Customize the progress bar color
+
+  loading: { color: '#FFFFFF' },
+
+  // Build configuration
+
+  build: {
+
+    // Run ESLint on save
+
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+
+  modules: [
+    '@nuxtjs/font-awesome',
+    '@nuxtjs/optimize'
+  ],
+
+  plugins: [
+    { src: '~plugins/webFontLoader.js', ssr: false },
+    '~/plugins/vue-scrollto.js'
+  ]
 }
